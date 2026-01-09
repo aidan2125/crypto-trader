db01ffaf23b2913d5710ca2745f0a540b970b5ae
Aidan Botha <aidanbotha15@gmail.com>
2026-01-09 03:12:37 +0200

    database

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/aidan/crypto-trader/.venv
+set -gx VIRTUAL_ENV /home/aidan/Desktop/crypto-trader/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
